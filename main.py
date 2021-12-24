5bd09bb18c50ce3b60eea47b9d4bd81c6d1a3a79
SHANTANU <43521260+shanu1997@users.noreply.github.com>
2021-12-24 19:15:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy
+def region(i,v):
+    m=numpy.zeros_like(i)
+    cv2.fillPoly(m,v,255)
+    return cv2.bitwise_and(i,m)
+def dl(i,l):
+    i=numpy.copy(i)
+    bi=numpy.zeros((i.shape[0],i.shape[1],3),dtype=numpy.uint8)
+    for line in l:
+        for x1,y1,x2,y2 in line:
+            cv2.line(bi,(x1,y1),(x2,y2),(255,255,255),thickness=1)
+    return cv2.addWeighted(i,0.5,bi,1,0)
+def main(i):
+    print('shape=',i.shape,'length=',len(i))
+    return dl(
+        i,cv2.HoughLinesP(
+        region(
+        cv2.Canny(
+            cv2.cvtColor
+            (i,cv2.COLOR_RGB2GRAY),100,150),
+        numpy.array(
+            [
+                [
+                    (0,i.shape[0]),(i.shape[1]/2,i.shape[0]/2),(i.shape[1],i.shape[0])
+                ]
+            ],numpy.int32
+        )
+    ),rho=1,theta=numpy.pi/180,threshold=80,lines=numpy.array([]),minLineLength=100,maxLineGap=100
+    )
+              )
+
+
+v=cv2.VideoCapture('road2.mp4')
+
+while v.isOpened():
+    r,f=v.read()
+    f=main(f)
+    cv2.imshow('Road Lane Detection',f)
+    if cv2.waitKey(0)&0xFF==ord(' '):
+        break
